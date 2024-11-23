bd6b79810b801a111bf73e2bc09c06c3fd3ba7ef
xingyu yao <xy2779@nyu.edu>
2024-11-22 22:51:39 -0500

    Update HW11.ino

--- HW11.ino
+++ HW11.ino
@@ -1,10 +1,21 @@
-//HW11
 void setup() {
-  // put your setup code here, to run once:
+  // Set each pin connected to LEDs as OUTPUT
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
 
+  // Turn all LEDs ON
+  digitalWrite(2, HIGH);
+  digitalWrite(3, HIGH);
+  digitalWrite(4, HIGH);
+  digitalWrite(5, HIGH);
+  digitalWrite(6, HIGH);
+  digitalWrite(7, HIGH);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  // No code needed in loop since the LEDs stay ON
 }
